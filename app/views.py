72c2f3e96bd5fbbf8c3e771a958b6d4989a08c23
Jacen Barefoot <89105120+JacenBarefoot@users.noreply.github.com>
2022-04-05 10:12:39 -0500

    Added a url going to Home2.html

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home2(request):
+	return render(request, 'Home2.html')
--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from app.views import *
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home2', home2, name="home2")
 ]
