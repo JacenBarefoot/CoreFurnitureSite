567f13b73e78217454755c8f1305f7fe89abb26c
Jacen Barefoot <89105120+JacenBarefoot@users.noreply.github.com>
2022-04-28 13:58:43 -0500

    Dylan changed the models to make it to where the item numbers could have more characters!

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Item(models.Model):
     type = models.CharField(max_length=50, default=None, null=True)
-    item_no = models.IntegerField(default=None, null=True)
+    item = models.CharField(max_length=100, default=None, null=True)
     image = models.ImageField(upload_to='images/', default=None, null=True)
 
     def __str__(self):
-        return str(self.item_no)
+        return str(self.item)
