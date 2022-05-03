eed9c6389ac59cf43b4659999cf9c5758d339084
Jacen Barefoot <89105120+JacenBarefoot@users.noreply.github.com>
2022-05-03 13:39:37 -0500

    Sync

--- config/urls.py
+++ config/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('hometest/', hometest, name="hometest"),
     path('hometest2/', hometest2, name="hometest2"),
     path('browse/<category>/', categories, name="categories"),
-    path('testimonieals/', testimonies, name="testimonials")
+    path('testimonials/', testimonies, name="testimonials")
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
